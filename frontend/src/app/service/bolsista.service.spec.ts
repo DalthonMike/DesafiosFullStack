c197c9ce7ece52d325940db569ce1cfe35a7a709
Dalthon Mike <dalthonmike13@gmail.com>
2024-01-23 11:54:11 -0300

    [FRONTEND] - Ajuste na validação do cadastro de bolsista no campo do banco.

--- frontend/src/app/service/bolsista.service.spec.ts
+++ frontend/src/app/service/bolsista.service.spec.ts
@@ -1,10 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { BolsistaService } from './bolsista.service';
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import {BolsistaModel} from "../model/bolsista.model";
 
 describe('BolsistaService', () => {
   let service: BolsistaService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +15,94 @@ describe('BolsistaService', () => {
     });
 
     service = TestBed.inject(BolsistaService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it('retorna uma string vazia para getPathPageable()', () => {
+    const result = service.getPathPageable();
+    expect(result).toBeDefined();
+    expect(result).toEqual('');
+  });
+
+  it('retorna uma lista de Bolsistass pelo método listarTodos ', inject([BolsistaService], (bolsistaService: BolsistaService) => {
+    const mockData: jasmine.Expected<jasmine.ArrayLike<any>> = [
+      {
+        "id": 1,
+        "nome": "Fulano",
+        "dataCadastro": [2022, 1, 1],
+        "numeroAgencia": 1234,
+        "numeroConta": 56789,
+        "identificador": "CPF",
+        "banco": "BANCO_DO_BRASIL",
+        "numeroIdentificador": 12345678901,
+        "pagamentos": [
+          {
+            "id": 1,
+            "idBolsista": 1,
+            "dataPagamento": [2022, 1, 10],
+            "valor": 1000.00,
+            "status": "NAO_REALIZADO"
+          },
+          {
+            "id": 2,
+            "idBolsista": 1,
+            "dataPagamento": [2022, 1, 15],
+            "valor": 1500.00,
+            "status": "CANCELADO"
+          }
+        ],
+        "atividade": "ATIVO"
+      },
+    ];
+
+    bolsistaService.listarTodos().subscribe(data => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne(`${bolsistaService.pathBase}/bolsista/todos`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockData);
+  }));
+
+  it('teste para chamada POST para cadastrar bolsista', inject([BolsistaService], (bolsistaService: BolsistaService) => {
+    const formData: BolsistaModel = {
+      id: 1,
+      codigoBanco: 1234,
+      nome: 'Registro teste',
+      numeroAgencia: 3214213,
+      numeroConta: 4123124,
+      identificador: 'CPF',
+      banco: 'BRADESCO',
+      numeroIdentificador: 20294891379,
+      dataCadastro: new Date(),
+    };
+
+    bolsistaService.cadastro(formData).subscribe(response => {
+      expect(response.status).toEqual(200); // Ajuste conforme o status esperado da resposta
+    });
+
+    const req = httpMock.expectOne(`${bolsistaService.pathBase}/bolsista/cadastrar`);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(formData); // Verifica se o corpo da solicitação é o esperado
+
+    req.flush({ status: 200 });
+  }));
+
+  it('Requisição do tipo POST para buscar bolsista pelo método buscarPorId', inject([BolsistaService], (bolsistaService: BolsistaService) => {
+    const bolsistaId = 1;
+
+    bolsistaService.buscarPorId(bolsistaId).subscribe(response => {
+      expect(response.status).toEqual(200);
+    });
+
+    const req = httpMock.expectOne(`${bolsistaService.pathBase}/bolsista/${bolsistaId}`);
+    expect(req.request.method).toEqual('POST');
+
+    req.flush({ status: 200, data: { /* ...dados do bolsista... */ } });
+  }));
 });
